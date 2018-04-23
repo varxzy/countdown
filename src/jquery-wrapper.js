5ab88a77cbb51586dfceee648e7643ffa2f87cf7
varxzy <varxzy@gmail.com>
2018-04-23 09:59:20 +0800

    修改文件缩进：2空格->4空格

--- src/jquery-wrapper.js
+++ src/jquery-wrapper.js
@@ -3,16 +3,16 @@ var NAME = 'countdown';
 var DATA_ATTR = 'date';
 
 jQuery.fn.countdown = function(options) {
-  return $.each(this, function(i, el) {
-    var $el = $(el);
-    if (!$el.data(NAME)) {
-      // allow setting the date via the data-date attribute
-      if ($el.data(DATA_ATTR)) {
-        options.date = $el.data(DATA_ATTR);
-      }
-      $el.data(NAME, new Countdown(el, options));
-    }
-  });
+    return $.each(this, function(i, el) {
+        var $el = $(el);
+        if (!$el.data(NAME)) {
+            // allow setting the date via the data-date attribute
+            if ($el.data(DATA_ATTR)) {
+                options.date = $el.data(DATA_ATTR);
+            }
+            $el.data(NAME, new Countdown(el, options));
+        }
+    });
 };
 
 module.exports = Countdown;
